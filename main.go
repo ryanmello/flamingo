872256b43e3e7d48e33a3b7b2ff078b671778df8
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-08-24 16:47:34 -0700

    create router and fouter functions

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
 func main(){
-	fmt.Println("Hello World")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		d, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error occuured", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(w, "%s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request){
+		log.Println("Goodbye world")
+	})
+
+	http.ListenAndServe(":9090", nil)
 }
