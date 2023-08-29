41afb65ddfa81517ad1d096604fc0a1c9cb2835b
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-08-28 19:15:17 -0700

    products data

--- data/products.go
+++ data/products.go
@@ -14,3 +14,24 @@ type Product struct {
 	UpdatedOn string
 	DeletedOn string
 }
+
+var productList = []*Product{
+	{
+		ID: 1,
+		Name: "Latte",
+		Description: "Frothy milky coffee",
+		Price: 2.45,
+		SKU: "00001",
+		CreatedOn: time.Now().UTC().String(),
+		UpdatedOn: time.Now().UTC().String(),
+	},
+	{
+		ID: 2,
+		Name: "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price: 1.95,
+		SKU: "00002",
+		CreatedOn: time.Now().UTC().String(),
+		UpdatedOn: time.Now().UTC().String(),
+	},
+}
