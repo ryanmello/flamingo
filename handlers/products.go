fc0ea1c9119a8dabc2e2f54ece96eddc5eadb0c3
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-08-29 19:38:35 -0700

    display data

--- handlers/products.go
+++ handlers/products.go
@@ -16,8 +16,8 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, r http.Request){
-	lp = data.GetProducts()
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request){
+	lp := data.GetProducts()
 	d, err := json.Marshal(lp)
 
 	if err != nil {
